f4dc87bc3b5f69d4d30f8480a0abcd9cac60d1de
jcgsr <cabralguerra@gmail.com>
2022-09-26 19:49:45 -0300

    timespan right

--- components/TimeSpan.js
+++ components/TimeSpan.js
@@ -11,7 +11,7 @@ export default function TimeSpan() {
     <div>
       {" "}
       <div className="timespan">
-        <p>from Tue, Jun 28, 2022 to</p>
+        <p>from Sun, Sep 25, 2022 to</p>
         <p id="date">{new Date().toLocaleDateString("en-US", DATE_OPTIONS)}</p>
       </div>
     </div>
